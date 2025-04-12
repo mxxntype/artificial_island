7654655d1d12a690260aa767896a143ca92c9c24
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-04-12 23:28:28 +0300

    fix: Context for erros

--- crates/neurotransmitter/src/server.rs
+++ crates/neurotransmitter/src/server.rs
@@ -1,4 +1,4 @@
-use epicentre_diagnostics::color_eyre::eyre::eyre;
+use epicentre_diagnostics::color_eyre::eyre::{Context, eyre};
 use epicentre_diagnostics::{DiagnosticLayer, Report, tracing};
 use iroh::Endpoint;
 use iroh::protocol::Router;
@@ -6,21 +6,24 @@ use iroh_blobs::net_protocol::Blobs;
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
-    DiagnosticLayer.setup()?;
+    DiagnosticLayer
+        .setup()
+        .wrap_err("Failed to setup the diagnostic layer")?;
 
     let endpoint = Endpoint::builder()
         .discovery_n0()
         .bind()
         .await
-        .map_err(|error| eyre!("{error}"))?;
+        .map_err(|error| eyre!("{error}"))
+        .wrap_err("Failed to create an Iroh endpoint")?;
     let blobs = Blobs::memory().build(&endpoint);
     let router = Router::builder(endpoint)
         .accept(iroh_blobs::ALPN, blobs)
         .spawn()
         .await
-        .map_err(|error| eyre!("{error}"))?;
-
-    tracing::info!("Router accept loop is ready, send SIGINT to shutdown");
+        .inspect(|_| tracing::info!("Router accept loop is ready, send SIGINT to shutdown"))
+        .map_err(|error| eyre!("{error}"))
+        .wrap_err("Failed to spawn Iroh router")?;
 
     tokio::signal::ctrl_c().await?;
     tracing::warn!("Caught SIGINT signal, shutting down");
