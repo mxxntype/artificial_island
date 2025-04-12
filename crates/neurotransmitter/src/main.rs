8ae7be014dbfdf3246f2bd18fb2dd8deca4dab00
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-04-12 19:27:43 +0300

    fix: Map `anyhow` errors to `eyre::Report`-compatible ones

--- crates/neurotransmitter/src/main.rs
+++ crates/neurotransmitter/src/main.rs
@@ -1,3 +1,4 @@
+use epicentre_diagnostics::color_eyre::eyre::eyre;
 use epicentre_diagnostics::{DiagnosticLayer, Report, tracing};
 use iroh::Endpoint;
 use iroh::protocol::Router;
@@ -7,20 +8,24 @@ use iroh_blobs::net_protocol::Blobs;
 async fn main() -> Result<(), Report> {
     DiagnosticLayer.setup()?;
 
-    let endpoint = Endpoint::builder().discovery_n0().bind().await.unwrap();
+    let endpoint = Endpoint::builder()
+        .discovery_n0()
+        .bind()
+        .await
+        .map_err(|error| eyre!("{error}"))?;
     let blobs = Blobs::memory().build(&endpoint);
     let router = Router::builder(endpoint)
         .accept(iroh_blobs::ALPN, blobs)
         .spawn()
         .await
-        .unwrap();
+        .map_err(|error| eyre!("{error}"))?;
 
     tracing::info!("Router accept loop is ready, send Ctrl+C to shutdown");
 
     tokio::signal::ctrl_c().await?;
     tracing::warn!("Caught Ctrl+C signal, shutting down");
 
-    router.shutdown().await.unwrap();
+    router.shutdown().await.map_err(|error| eyre!("{error}"))?;
 
     Ok(())
 }
